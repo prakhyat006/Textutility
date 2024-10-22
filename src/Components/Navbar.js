8569788431dd2c4721fd8a2d0817824540cf4c74
prakhyat006 <prakhyatshetty710@gmail.com>
2024-10-22 22:53:39 +0530

    Removed about and added #

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -5,17 +5,17 @@ export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg bg-${props.mode} navbar-${props.mode}`}>
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">{props.t}</a>
+    <a className="navbar-brand" href="#">{props.t}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="/">{props.h}</a>
+          <a className="nav-link active" aria-current="page" href="#">{props.h}</a>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="/">{props.a}</a>
+          <a className="nav-link" href="#">{props.a}</a>
         </li>
       </ul>
       <div class={`form-check form-switch text-${props.mode==="light"? "dark":"light"}`}>
